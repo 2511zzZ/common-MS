ebc1e48d7c075587e6e60b3332cf483d28e77d91
王周 <2511672322@qq.com>
2020-06-10 20:44:09 +0800

    SpringBoot init

---
+++ src/main/java/priv/zzz/mscommon/MSCommonApplication.java
@@ -0,0 +1,13 @@
+package priv.zzz.mscommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MSCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MSCommonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/priv/zzz/mscommon/MSCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package priv.zzz.mscommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MSCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
